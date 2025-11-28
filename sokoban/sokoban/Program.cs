e4e0e9dd9075776d312ba6f3e3746930d23349f8
kimin <kimin896@gmail.com>
2025-11-28 10:47:33 +0900

    feat : GameReset

--- sokoban/sokoban/Program.cs
+++ sokoban/sokoban/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 게임 초기화 
+
+            Console.ResetColor();
+            // 색깔 조정
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Green;
+            // 제목 수정
+            Console.Title = "My Sokoban,";
+            // 커서 숨김
+            Console.CursorVisible = false;
+            Console.WriteLine(Console.ReadLine());
+            Console.Clear(); 
+
+
         }
     }
 }
