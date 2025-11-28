7b868f9753fd61cff9e8f3ba9e7004be4fcbed21
kimin <kimin896@gmail.com>
2025-11-28 14:50:58 +0900

    feat : 플레이어 이동 구현

--- sokoban/sokoban/Program.cs
+++ sokoban/sokoban/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace sokoban
+﻿using System.Numerics;
+
+namespace sokoban
 {
     internal class Program
     {
@@ -7,16 +9,39 @@
             // 게임 초기화 
             Console.ResetColor();
             // 색깔 조정
-            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.BackgroundColor = ConsoleColor.Magenta;
             Console.ForegroundColor = ConsoleColor.Green;
             // 제목 수정
             Console.Title = "My Sokoban,";
             // 커서 숨김
             Console.CursorVisible = false;
-            Console.WriteLine(Console.ReadLine());
-            Console.Clear(); 
+            Console.Clear();
+            int playerX = 0;
+            int playerY = 0;
+            string player = "A";
 
-
+            while (true)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write(player);
+                ConsoleKey input = Console.ReadKey().Key;
+                switch (input)
+                {
+                    case ConsoleKey.DownArrow:
+                        playerY++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        playerY--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        playerX++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        playerX++;
+                        break;
+                }
+            }
         }
     }
 }
